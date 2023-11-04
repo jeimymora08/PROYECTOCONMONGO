7afd53e46f52432dab132916b5b24d5d12445cec
jeimymora08 <jeimyandrea@gmail.com>
2023-11-04 11:15:27 -0500

    creacion de usuarios

--- src/Models/users.js
+++ src/Models/users.js
@@ -1,9 +1,43 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
 
-const CompradoresSchemma = new mongoose.Schema({
-  name: String,
+const UserSchemma = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  identification_number: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone_number: {
+    type: Number,
+    required: true,
+   },
+   products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Producto" }], //productos por usuario//
 });
 
-const Usuario = mongoose.model("Usuario", CompradoresSchemma);
-
-module.exports = Usuario;
+UserSchemma.statics.encryptPassword = async (password)=>{
+  const salt = await bcrypt.genSalt(10) //cantidad de rondas que va a dar el encriptado a mi password
+  return await bcrypt.hash(password,salt) //=> 153asd46wer6asfg (devuelve algo encriptado)
+  }
+  
+  UserSchemma.statics.comparePassword = async (password, receivedPassword)=>{
+  return await bcrypt.compare(password, receivedPassword)  //=> true o false
+  }
+  
+  const User = mongoose.model("User", UserSchemma);
+  
+  module.exports = User;
+  
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ require("./db.js");
 app.use(express.json()); 
 app.use(cors());
 const Producto = require("./Models/productos"); 
-const Usuario = require ("./Models/Users.js");
+const User = require ("./Models/Users.js");
 
 //rutas del programa//
 
@@ -22,13 +22,44 @@ app.post("/Producto", async  (req, res) => {
 });
 
 
-app.post("/Usuario", async  (req, res) => {
-  const name = req.query.name;
-  let usuario = new Usuario ({name : name});
-  const nuevoUsuario =  await  Usuario.create({ name: name });
-  res.json("Usuario creado correctamente");
-});
+app.post("/user", async (req, res) => { 
+  const {
+    username,
+    email,
+    identification_number,
+    password,
+    phone_number,
+    products, // ["Roberto", "Esteban"]
+  } = req.body;
 
+  const productsFound = await Producto.find({ name: { $in: products } });
+  console.log("productsFound",productsFound);
+
+
+
+
+
+  /* crea"r la instancia del usuario */
+  const user = new User({
+    username: username,
+    email: email,
+    identification_number: identification_number,
+    password: password, //123456 =>cambia por el hash de encryptPassword()
+    phone_number: phone_number,
+    products: productsFound.map((product) => product._id), //=> [new ObjectId("651cb3a8f62c611047c7be57"),new ObjectId("651cb3b5f62c611047c7be59"),]
+  });
+  /* Debemos encriptar la contraseña */
+  user.password = await User.encryptPassword(password);
+
+  /* Guardo en la base de datos */
+  const newUser = user.save();
+ 
+  res.status(200).json({
+    _id: newUser._id,
+    username: newUser.username,
+    mail: newUser.email,
+  });
+});
 
 
 module.exports = { app, port };

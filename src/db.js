673ecbf688100df4407119ffd2fb125d2bedc57a
jeimymora08 <jeimyandrea@gmail.com>
2023-11-13 17:37:02 -0500

    lunes2

--- src/db.js
+++ src/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose"); /* importo el módulo de mongoose */
 
 async function connection() {
-  await mongoose.connect(
+  await mongoose
+    .connect(
       "mongodb+srv://jeimyandrea08:19541985@loginmygconfecciones.heawbcm.mongodb.net/?retryWrites=true&w=majority"
     )
     .then(console.log("Base de datos Mongo conectada"))
-    .catch(err) {
-      console.error("error al conectar con el servidor:",err);
-    }
+    .catch((err) => console.log(err));
 }
 
 module.exports = { connection };
